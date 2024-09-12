a41be69754d16b44b868965a6ca7971a18530768
Mark Wayne Menorca <markwayne.menorca@outlook.com>
2024-09-12 23:05:32 +0800

    chore: update code

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setup(
     install_requires=[
         "paramiko",
         "tqdm",
-        "colorama",
-        "shutil"
+        "colorama"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
