10d054271b20d2bbdfe606b12c0e039dcf287d92
Mark Wayne Menorca <markwayne.menorca@outlook.com>
2024-09-12 14:24:54 +0800

    ignore: added  .scpignore

---
+++ test.py
@@ -0,0 +1,12 @@
+from pydaffodil import Daffodil
+
+cli = Daffodil(remote_user="root", remote_host="203.161.53.228", remote_path="/root/prod")
+
+steps = [
+    # {"step": "Build the project", "command": lambda: cli.run_command("npm run build")},
+    # {"step": "Stop backend process on remote server", "command": lambda: cli.make_directory("cams")},
+    # {"step": "Transfer files to remote server", "command": lambda: cli.transfer_files(local_path="dist",destination_path="/root/prod/cams")},
+    # {"step": "Restart backend process on remote server", "command": lambda: cli.ssh_command("sudo forever restartall")}
+]
+
+cli.deploy(steps)
